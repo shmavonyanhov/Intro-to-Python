6bb79ed032a8d0e31aaf8927a507557b5bf704a1
shmavonyanhov <shmavonyanhov@gmail.com>
2019-08-18 13:40:41 +0400

    added

---
+++ practical 1/problem 3.py
@@ -0,0 +1,6 @@
+course='Intro to Python' 
+student='Hovhannes Shmavonyan'
+nickname='nicknamerefuser'
+grade=15
+pythonExpert=True
+print("\n",'course:',course,"\n","student:",student,"\n","nickname:",nickname,"\n","grade:",grade,"\n","python expert:",pythonExpert)
---
+++ practical 1/problem 5.py
@@ -0,0 +1,5 @@
+import math
+AB =3
+AC =4
+BC=math.sqrt(AB**2+AC**2)
+print("The hypotenuse of trinagle ABC",BC)
