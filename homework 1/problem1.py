1fafb8676b257832f96fa1bb3e179c92291f45e4
shmavonyanhov <shmavonyanhov@gmail.com>
2019-08-19 14:42:48 +0400

    home1

---
+++ homework 1/problem1.py
@@ -0,0 +1,17 @@
+project="cake"
+difficulty=3
+ingredients=["flour","butter","sugar","eggs","cocoa powder","baking powder"]
+print("apples" in ingredients)
+print("butter" in ingredients)
+print(("eggs"in ingredients) or ("margarine" in ingredients))
+print(("eggs"in ingredients) and ("margarine" in ingredients))
+flour=175
+butter=175
+sugar='100g'
+eggs=2
+cocoa_powder='1ts'
+baking_powder=0.5
+
+print(type(flour),type(butter),type(sugar),type(eggs),type(cocoa_powder),type(baking_powder))
+
+print("\n flour-",flour,"\n butter-",butter,"\n sugar-",sugar,"\n eggs-",eggs,"\n cocoa powder-",cocoa_powder,"\n baking powder-",baking_powder)
